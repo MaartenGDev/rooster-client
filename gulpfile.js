2908c1c18757b7905baa47039d89e3aa9ca15c15
MaartenGDev <maarten.grootoonk@gmail.com>
2016-09-04 14:07:33 +0200

    Added browserify for react

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,41 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./src/sass/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+var browserify = require('gulp-browserify');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+
+
+gulp.task('browserify', function() {
+	var production = gutil.env.type === 'production';
+
+	gulp.src(['src/js/index.js'], {read: false})
+
+		.pipe(browserify({
+			debug: !production,
+			transform: ['reactify'],
+			extensions: ['.jsx']
+		}))
+
+		.on('prebundle', function(bundler) {
+			bundler.require('react');
+		})
+
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest('dist/js/'));
 });
- 
+
+gulp.task('sass', function () {
+	return gulp.src('./src/sass/main.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('js:watch',function(){
+	gulp.watch('src/js/index.js', ['browserify']);
 });
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var App = React.createClass({
+  render: function(){
+    return (
+            <h1>test443</h1>
+    )
+  }
+});
+
+ReactDOM.render(<App />, document.querySelector('.app'));
